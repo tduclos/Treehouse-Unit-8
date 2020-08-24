47bdf32cb9aa9bfc63dd40f98b91ecd7a0e483f5
tduclos <67517520+tduclos@users.noreply.github.com>
2020-08-24 16:18:42 -0400

    cleanup

--- app.js
+++ app.js
@@ -13,10 +13,8 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-//app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-//app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
