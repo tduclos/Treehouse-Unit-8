45e5eedd781588785544e16b0b039832734205f8
tduclos <67517520+tduclos@users.noreply.github.com>
2020-08-24 11:52:23 -0400

    Routes set up
    Routes are currently empty dummy routes, I'll sort them out after the pug files. Also removed some unnessecare dependancies from the code I borrowed from Guil's workshop

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+//This code is mostly from Guil's Unit 8 Workshop.
+
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 
-const routes = require('./routes/index');
-const articles = require('./routes/articles');
+const indexRouter = require('./routes/index');
+const booksRouter = require('./routes/books');
 
 const app = express();
 
@@ -11,28 +13,32 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
+//app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+//app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/articles', articles);
+app.use('/', indexRouter);
+app.use('/books', booksRouter);
 
 // catch 404 and forward to error handler
-app.use( (req, res, next) => {
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use( (err, req, res, next) => {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  if(err.status === 404) {
+    res.status(404).render('page-not-found');
+  } else {
+    res.status(err.status || 500).render('error', {err});
+  }
 });
 
 module.exports = app;
---
+++ routes/books.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const router = express.Router();
+const Book = require('../models').Book;
+
+/* Handler function to wrap each route. */
+function asyncHandler(cb){
+  return async(req, res, next) => {
+    try {
+      await cb(req, res, next)
+    } catch(error){
+      res.status(500).send(error);
+      console.log('Async Handler error')
+      res.render('error');
+    }
+  }
+}
+
+
+/* GET books table. */
+router.get('/', asyncHandler(async (req, res) => {
+  
+}));
+
+/* Create book form */
+router.get('/new', (req, res) => {
+  
+});
+
+/* Create book (POST) */
+router.post('/new', asyncHandler(async (req, res) => {
+  
+}));
+
+
+/* Update book form */
+router.get("/:id", asyncHandler(async(req, res) => {
+  
+}));
+
+
+/* Update book. */
+router.post('/:id', asyncHandler(async (req, res) => {
+  
+}));
+
+
+/* Remove book */
+router.post('/:id/delete', asyncHandler(async (req ,res) => {
+ 
+}));
+
+module.exports = router;
